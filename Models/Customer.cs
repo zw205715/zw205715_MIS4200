93318a4d9964cbac6ea095b59a7e98e4945ec209
Zachary Whetstone <zw205715@ohio.edu>
2019-09-11 11:39:48 -0400

    commit

---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zw205715_MIS4200.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+
+        public ICollection<Orders> orders { get; set; }
+    }
+}
---
+++ Models/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zw205715_MIS4200.Models
+{
+    public class Orders
+    {
+        public int orderID { get; set; }
+
+        public int customerID { get; set; }
+        public Customer customer { get; set; }
+
+        public DateTime orderDate { get; set; }
+
+    }
+}
